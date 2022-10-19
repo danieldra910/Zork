9a93bf7763022b094855d66be8b6ea3065a548b9
danieldra910 <daniel_arias2000@knights.ucf.edu>
2022-10-19 13:46:43 -0400

    deleted comments

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using Newtonsoft.Json;
 
 namespace Zork
 {
@@ -9,23 +6,13 @@ namespace Zork
     {
         private static void Main(string[] args)
         {
-            //Game game = JsonConvert.DeserializeObject<Game>(File.ReadAllText(@"Content\Game.json"));
-            
             string roomsFileName = (args.Length > 0) ? args[(int)CommandLineArguments.GameFilename] : @"Content\Game.json";
 
             Game game = Game.Load(roomsFileName);
 
             Console.WriteLine("Welcome to Zork!");
             game.Run();
-            
-            
         }
-
-        //private enum Fields
-        //{
-        //    Name = 0,
-        //    Description 
-        //}
 
         private enum CommandLineArguments
         {
